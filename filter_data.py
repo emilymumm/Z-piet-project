fadf912c762c833cd625f7210a318314117d2735
Emily Mumm <NA>
2019-01-16 17:51:19 +0100

    final touches

--- filter_data.py
+++ filter_data.py
@@ -6,7 +6,7 @@ import time
 
 start_time = time.time()
 run_number = 1
-files_per_run = 10
+files_per_run = 10000
 
 
 range_high = run_number * files_per_run
@@ -17,9 +17,9 @@ print(range_low)
 
 
 #input twitter data
-my_path = '/Volumes/backup/dutch_data/sort_users_dutch'  
+my_path = '/Volumes/backup/aws_data/z_piet'  
 ##create file to save data
-outfile = '/Volumes/backup/dutch_data/engagement_stat/' ## CHANGE ME
+outfile = '/Volumes/backup/aws_data/z_piet_filter/' ## CHANGE ME
 
 
 files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
@@ -59,8 +59,8 @@ for file_no in range(range_low, range_high):
 print("How many tweets: " + str(len(all_tweets)))
 
 
-zpiet_text_list = ['zwarte piet', 'black piet', '#zwartepiet', '#zwartepieten', '#blackpiet']
-zpiet_hashtag_list = ['#zwartepiet', '#zwartepieten', '#blackpiet']
+zpiet_text_list = ['zwarte piet', 'black piet', 'zwartepiet', 'zwartepieten' , '#zwartepiet', '#zwartepieten', '#blackpiet']
+zpiet_hashtag_list = ['#zwartepiet', '#zwartepieten', '#blackpiet', 'zwartepiet', 'zwartepieten', 'blackpiet']
 zwarte_text = ['zwarte', 'black']
 piet_text = ['piet', 'pieten']
 verified_no_piet = []
@@ -76,8 +76,35 @@ def is_verified(tweet):
 	else:
 		return False
 
-def is_zpiet_related(tweet):
-	text = tweet['text']
+
+def is_retweet(tweet):
+	if 'retweeted_status' in tweet.keys():
+		return True
+	else:
+		return False
+
+
+def get_text(tweet):
+	try:
+		text = tweet['retweeted_status']['extended_tweet']['full_text'].lower()
+	except:
+		try:
+			text = tweet['retweeted_status']['quoted_status']['extended_tweet']['full_text'].lower()	
+		except:
+			try: 
+				text = tweet['quoted_status']['text'].lower()
+			except:
+				text = tweet['text'].lower()
+
+	return text	
+
+	# if is_retweet(tweet):
+
+
+	# text = tweet['text'].lower()
+	# hashtag = get_hashtag(tweet)
+
+def is_zpiet_related(text):
 	if ('zwarte' and 'piet') in text:
 		return True
 	elif ('black' and 'piet') in text:
@@ -85,18 +112,23 @@ def is_zpiet_related(tweet):
 	for word in text:
 		if word in zpiet_text_list:
 			return True
+	
 
 
 for tweet in all_tweets:
+	text = get_text(tweet)
+
 	if is_verified(tweet):
-		if is_zpiet_related(tweet):
+		if is_zpiet_related(text):
 			verified_piet.append(tweet)
 		else:
 			verified_no_piet.append(tweet)
-	elif is_zpiet_related(tweet):
+	elif is_zpiet_related(text):
 		activist_piet.append(tweet)
 	else:
 		activist_no_piet.append(tweet)
+
+
 
 print("total verified no piet: " + str(len(verified_no_piet)))
 print("total verified piet: " + str(len(verified_piet)))
@@ -115,6 +147,9 @@ with open(outfile + 'activist_no_piet' + str(run_number) + '.json', 'w') as f:
 
 with open(outfile + 'activist_piet' + str(run_number) + '.json', 'w') as f:
 	json.dump(activist_piet, f)
+
+
+
 
 
 print('done')
